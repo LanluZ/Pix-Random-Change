ed3f23820f23f78b33ad8df86dfff54ed5311841
LanluZ <LanluZ@qq.com>
2023-07-19 17:25:07 +0800

    修改 随机范围

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
 
     # Image转cv2
     img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-    img[0, 0, 0] = random.randint(0, 255)
+    img[0, 0, 0] = random.randint(0, 0xffffff)
 
     # cv2转Image
     img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
