6da31777af2846d26768dfa839071e49b5a52b25
LanluZ <LanluZ@qq.com>
2023-07-19 16:18:43 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import cv2
+import random
+import ImgTextClipboard
+import numpy as np
+
+from PIL import Image
+
+
+def main():
+    # 读入剪贴板图片
+    img = ImgTextClipboard.copyImgFormClipboard()
+
+    # Image转cv2
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    img[0, 0, 0] = random.randint(0, 255)
+
+    # cv2转Image
+    img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+    # 写入图片到剪贴板
+    ImgTextClipboard.pasteImgToClipboard(img)
+
+
+if __name__ == '__main__':
+    main()
