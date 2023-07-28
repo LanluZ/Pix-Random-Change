083094433a43c452bee838fdc61a182cd877e29c
LanluZ <LanluZ@qq.com>
2023-07-28 12:50:16 +0800

    添加 文件修改方法

--- module.py
+++ module.py
@@ -11,3 +11,10 @@ def pixRandomChange(img):
     # cv2转Image
     img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     return img
+
+
+def pixRandomChangeFile(path):
+    # 注意：cv2.imread()不支持路径中文
+    img = cv2.imread(path)
+    img[0, 0, 0] = random.randint(0, 0xfffffff)
+    cv2.imwrite(path, img)
